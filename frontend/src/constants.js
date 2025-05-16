54ea90d48201b7ab024029c1fcae4c72c36f8192
versevibes96 <versevibes96@gmail.com>
2025-05-16 22:39:25 +0530

    Create constants.js

---
+++ frontend/src/constants.js
@@ -0,0 +1,10 @@
+export const QUALITY_OPTIONS = [
+  { value: '144', label: '144p' },
+  { value: '240', label: '240p' },
+  { value: '360', label: '360p' },
+  { value: '480', label: '480p' },
+  { value: '720', label: '720p HD' },
+  { value: '1080', label: '1080p HD' },
+];
+
+export const API_URL = 'http://localhost:5000/api'; // Replace with your actual API
