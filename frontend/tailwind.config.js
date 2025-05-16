3b47ac9952917366088cadbb1732ebc46b4ad473
versevibes96 <versevibes96@gmail.com>
2025-05-16 22:48:04 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#f87171',
+          dark: '#dc2626',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
